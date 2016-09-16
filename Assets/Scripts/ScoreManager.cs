0584c99d4f65481094be4ab42ea508675831b930
tsarachan <tes353@nyu.edu>
2016-09-16 11:13:58 -0400

    Score feedback implemented
    When tokens are removed, the points earned appear at their locations.

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -10,14 +10,24 @@ public class ScoreManager : MonoBehaviour {
 	public int maxScore = 9999;
 
 	int basicIncrement = 1;
-	public int BasicIncrement { get; set; }
+	public int BasicIncrement {
+		get { return basicIncrement; }
+		set { basicIncrement = value; }
+	}
 
 	GameObject scoreFeedback;
+
+	float canvasX;
+	float canvasY;
 
 	void Start() {
 		score = GetComponent<Text>();
 		score.text = currentScore.ToString();
 		scoreFeedback = Resources.Load("Score feedback") as GameObject;
+
+		RectTransform overlayCanvas = transform.root.Find("Score canvas").GetComponent<RectTransform>();
+		canvasX = overlayCanvas.anchoredPosition.x;
+		canvasY = overlayCanvas.anchoredPosition.y;
 	}
 
 	public void UpdateScore(int amount) {
@@ -31,8 +41,17 @@ public class ScoreManager : MonoBehaviour {
 	}
 
 	public void LocalizedFeedback(int amount, Vector3 loc) {
-		GameObject newScoreFeedback = Instantiate(scoreFeedback, loc, Quaternion.identity) as GameObject;
-		newScoreFeedback.transform.SetParent(transform.root.Find("Score feedback canvas"), false);
+		GameObject newScoreFeedback = Instantiate(scoreFeedback,
+												  new Vector3(0.0f, 0.0f, 0.0f),
+												  Quaternion.identity) as GameObject;
+		
+		Vector3 pos = Camera.main.WorldToScreenPoint(loc);
+		Vector3 correctedPos = new Vector3(pos.x - canvasX,
+										   pos.y - canvasY,
+										   pos.z);
+		newScoreFeedback.GetComponent<RectTransform>().anchoredPosition = correctedPos;
+
+		newScoreFeedback.transform.SetParent(transform.root.Find("Score canvas"), false);
 		newScoreFeedback.GetComponent<Text>().text = amount.ToString();
 	}
 
